56ce561884321288169e041564e62b423a211375
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2021-05-03 23:36:24 +0300

    🔨Added Cheat Mode

---
+++ Dungeons of Probability/Assets/Scripts/CheatMode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatMode : MonoBehaviour
+{
+    MoveCamera moveCamera;
+    GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveCamera = GetComponent<MoveCamera>();
+        gameManager = GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M)){
+            moveCamera.Move();
+        }
+
+        if(Input.GetKeyDown(KeyCode.E)){
+            if(gameManager.Enemy.active == true){
+                gameManager.Enemy.gameObject.SetActive(false);
+            }
+            else{
+                gameManager.Enemy.gameObject.SetActive(true);
+            }
+        }
+    }
+}
