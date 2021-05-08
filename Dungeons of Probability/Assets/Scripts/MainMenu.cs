bb317f59a13283de534ec6a5b2f8c88d36c9c63f
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2021-05-08 12:02:50 +0300

    🔨Added Sound Button Functions

--- Dungeons of Probability/Assets/Scripts/MainMenu.cs
+++ Dungeons of Probability/Assets/Scripts/MainMenu.cs
@@ -6,12 +6,19 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Button changeLanguageButton;
+    public Button changeVolumeButton;
     public Sprite[] changeLanguageButtonStatuses;
+    public Sprite[] changeVolumeButtonStatuses;
     public LocalizationManager LocalizationManager;
+    public SetSound setSound;
     void Awake()
     {
         LocalizationManager = (LocalizationManager)FindObjectOfType(typeof(LocalizationManager));
         UpdateLanguageButtonStatus();
+        if(PlayerPrefs.HasKey("Volume") == false){
+            PlayerPrefs.SetInt("Volume", 1);
+        }
+        UpdateVolumeButtonStatus();
     }
 
     void Start()
@@ -44,5 +51,17 @@ public class MainMenu : MonoBehaviour
         {
             changeLanguageButton.GetComponent<Image>().sprite = changeLanguageButtonStatuses[1];
         }
+    }
+
+    public void ChangeSoundVolume(){
+        int currentVolume = PlayerPrefs.GetInt("Volume");
+        PlayerPrefs.SetInt("Volume", 1 - currentVolume);
+        setSound.ChangeAudioListenerVolume();
+        UpdateVolumeButtonStatus();
+    }
+
+    void UpdateVolumeButtonStatus(){
+        int currentVolume = PlayerPrefs.GetInt("Volume");
+        changeVolumeButton.GetComponent<Image>().sprite = changeVolumeButtonStatuses[currentVolume];
     }
 }
---
+++ Dungeons of Probability/Assets/Scripts/SetSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeAudioListenerVolume();
+    }
+
+    public void ChangeAudioListenerVolume()
+    {
+        int newVolume = PlayerPrefs.GetInt("Volume");
+        AudioListener.volume = newVolume;
+    }
+}
