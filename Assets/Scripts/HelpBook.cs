4db40418b226b2d38849cb566f737199a57a4068
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2021-06-19 16:09:24 +0300

    Added Basic Logic for Help Book

---
+++ Assets/Scripts/HelpBook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpBook : MonoBehaviour
+{
+    public List<Image> pages = new List<Image>();
+    int currentIndex = 0;
+
+    public void OpenPageByRelativeIndex(int relevantIndex){
+        ClosePages();
+        pages[currentIndex + relevantIndex].gameObject.SetActive(true);
+        currentIndex += relevantIndex;
+    }
+
+    public void OpenPageByIndex(int index){
+        ClosePages();
+        pages[index].gameObject.SetActive(true);
+        currentIndex = index;
+    }
+
+    void ClosePages(){
+        for (int i = 0; i < pages.Count; i++){
+            pages[i].gameObject.SetActive(false);
+        }
+    }
+}
