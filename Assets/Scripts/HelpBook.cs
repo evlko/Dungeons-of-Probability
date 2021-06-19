9215b9253161db3d8c55ad90e0dab96f233ed585
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2021-06-20 01:24:15 +0300

    🔨Added page button logic on help book borders

--- Assets/Scripts/HelpBook.cs
+++ Assets/Scripts/HelpBook.cs
@@ -7,22 +7,36 @@ public class HelpBook : MonoBehaviour
 {
     public List<Image> pages = new List<Image>();
     int currentIndex = 0;
+    public Transform[] PageSwithchers;
 
     public void OpenPageByRelativeIndex(int relevantIndex){
-        ClosePages();
-        pages[currentIndex + relevantIndex].gameObject.SetActive(true);
         currentIndex += relevantIndex;
+        OpenCurrentPage();
     }
 
-    public void OpenPageByIndex(int index){
-        ClosePages();
-        pages[index].gameObject.SetActive(true);
-        currentIndex = index;
+    public void OpenPageByIndex(int globalIndex){
+        currentIndex = globalIndex;
+        OpenCurrentPage();
     }
 
-    void ClosePages(){
+    void OpenCurrentPage(){
         for (int i = 0; i < pages.Count; i++){
             pages[i].gameObject.SetActive(false);
+        }
+        pages[currentIndex].gameObject.SetActive(true);
+        SetPageSwichers();
+    }
+
+    void SetPageSwichers(){
+        if (currentIndex == 0){
+            PageSwithchers[0].gameObject.SetActive(false);
+        }
+        else if(currentIndex == pages.Count - 1){
+            PageSwithchers[1].gameObject.SetActive(false);
+        }
+        else{
+            PageSwithchers[0].gameObject.SetActive(true);
+            PageSwithchers[1].gameObject.SetActive(true);
         }
     }
 }
